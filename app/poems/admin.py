dd4f6b41a877e8538ea21b7150d4c7186c5449e6
Olivier Dorvelus <ollydorvelus@gmail.com>
2021-05-31 22:43:19 -0400

    poems admin and model retouch

--- app/poems/admin.py
+++ app/poems/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from app.poems import models
+
 
 # Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    fields = ['name', 'active']
+
+    class Meta:
+        model = models.Genre
+
+
+class TopicAdmin(admin.ModelAdmin):
+    fields = ['name', 'active']
+
+    class Meta:
+        model = models.Topic
+
+
+class PoemAdmin(admin.ModelAdmin):
+    fields = ['user', 'title', 'genre', 'content', 'topics', 'likes', 'active']
+
+    class Meta:
+        model = models.Poem
+
+
+admin.site.register(models.Genre, GenreAdmin)
+admin.site.register(models.Topic, TopicAdmin)
+admin.site.register(models.Poem, PoemAdmin)
--- app/poems/models.py
+++ app/poems/models.py
@@ -12,16 +12,25 @@ class ActiveMixin(models.Model):
 class Poem(AbstractModel, ActiveMixin):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='poems')
     genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True, blank=False, related_name='poems')
-    topic = models.ManyToManyField('Topic', blank=False, related_name='poems')
-    likes = models.ManyToManyField('users.User', related_name='liked', symmetrical=False)
+    topics = models.ManyToManyField('Topic', blank=False, related_name='poems')
+    likes = models.ManyToManyField('users.User', related_name='liked')
     title = models.CharField(max_length=100, blank=False)
     content = models.TextField(blank=False, max_length=1000)
     about = models.CharField(max_length=150, blank=True, default='')
+
+    def __str__(self):
+        return f'{self.user} - {self.title}'
 
 
 class Genre(AbstractModel, ActiveMixin):
     name = models.CharField(max_length=55)
 
+    def __str__(self):
+        return self.name
+
 
 class Topic(AbstractModel, ActiveMixin):
     name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
