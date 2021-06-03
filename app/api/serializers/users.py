faeb75270ae6005e6bf86282be7a318602b2de95
Olivier Dorvelus <ollydorvelus@gmail.com>
2021-06-02 23:34:47 -0400

    user create

--- app/api/serializers/users.py
+++ app/api/serializers/users.py
@@ -28,3 +28,48 @@ class UserSerializer(serializers.ModelSerializer):
         if instance.profile:
             count = instance.profile.subscriber_count
         return count
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+    password2 = serializers.CharField(min_length=8, trim_whitespace=False, style={'input_type': 'password'},
+                                      write_only=True)
+    email2 = serializers.EmailField(trim_whitespace=True, style={'input_type': 'email'}, write_only=True)
+
+    class Meta:
+        model = User
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8, 'style': {'input_type': 'password'}}}
+        fields = ('email', 'email2', 'pen_name', 'password', 'password2')
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        email = attrs.get('email')
+        email2 = attrs.get('email2')
+
+        if password != password2:
+            msg = {'password2': ['Passwords must match.']}
+            raise serializers.ValidationError(msg)
+
+        if email != email2:
+            msg = {'email2': ['Emails must match.']}
+            raise serializers.ValidationError(msg)
+
+        return attrs
+
+    def create(self, validated_data):
+        """"Create a new user with encrypted password and return it"""
+        del validated_data['password2']
+        del validated_data['email2']
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- app/api/views/users.py
+++ app/api/views/users.py
@@ -1,8 +1,15 @@
 from rest_framework import viewsets
-from app.api.serializers.users import UserSerializer
+from app.api.serializers.users import UserSerializer, UserCreateSerializer
 from app.users.models import User, UserProfile
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        return UserSerializer
+
+
