a595c349abb016b5fba61f3b45487e13da6e8978
Olivier Dorvelus <ollydorvelus@gmail.com>
2021-06-07 22:43:01 -0400

    adding liked and subscribe views

--- app/api/views/poems.py
+++ app/api/views/poems.py
@@ -5,10 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from app.poems.models import Poem, Genre, Topic
+from app.users.models import User
 from app.api.serializers.poems import PoemSerializer, GenreSerializer, TopicSerializer, PoemCreateSerializer
 from app.api.permissions import UserObjectPermission
 from app.api.pagination import StandardResultsSetPagination
-
 
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
@@ -65,3 +65,21 @@ class PoemViewSet(viewsets.ModelViewSet):
         poems = PoemSerializer(user_poems, many=True).data
         return Response(poems)
 
+    @action(detail=True, methods=['GET'], name='Like Poem', permission_classes=[IsAuthenticated])
+    def subscribe(self, request, pk=None):
+        poem = self.get_object()
+        user = request.user
+        print(user)
+        user.like_poem(poem)
+        return Response(PoemSerializer(poem).data)
+
+    @action(detail=False, methods=['GET'], name='Liked poems', permission_classes=[IsAuthenticated])
+    def liked(self, request):
+        user_id = request.query_params.get('user_id', None)
+        if user_id:
+            user = User.objects.get(id=user_id)
+            poems = user.liked.filter(active=True)
+            serialized_poems = PoemSerializer(poems, many=True)
+            return Response(serialized_poems.data)
+        else:
+            return Response({'error': 'User id must be included in param'}, status=status.HTTP_400_BAD_REQUEST)
